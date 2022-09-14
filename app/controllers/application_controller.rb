b7691e944999eb3b374f0566771d3a4e2e64bb42
Israel Molestina <molestinaisrael@gmail.com>
2022-09-14 14:32:55 -0400

    added about page

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# class for rendering hello world
+class ApplicationController < ActionController::Base
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# class for rendering hello world
+class PagesController < ApplicationController
+  def home
+  end
+  
+  def about
+  end
+end
